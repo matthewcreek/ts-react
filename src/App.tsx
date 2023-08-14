c59f6862738766977dc21d3c5a1c28eb129cd2bd
Matthew Creek <matthew.creek27@gmail.com>
2023-08-14 16:36:21 -0500

    added code to only allow specific strings as prop values

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,41 @@
 import React from 'react';
-import Greet from './components/Greet';
-import Person from './components/Person';
-import PersonList from './components/PersonList';
+// import Greet from './components/Greet';
+// import Person from './components/Person';
+// import PersonList from './components/PersonList';
+import Status from './components/Status';
 
 import './App.css';
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne'
-  }
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne'
-    },
-    {
-      first: 'Clark',
-      last: 'Kent'
-    },
-    {
-      first: 'Princess',
-      last: 'Diana'
-    },
-  ]
+  // const personName = {
+  //   first: 'Bruce',
+  //   last: 'Wayne'
+  // }
+  // const nameList = [
+  //   {
+  //     first: 'Bruce',
+  //     last: 'Wayne'
+  //   },
+  //   {
+  //     first: 'Clark',
+  //     last: 'Kent'
+  //   },
+  //   {
+  //     first: 'Princess',
+  //     last: 'Diana'
+  //   },
+  // ]
 
   return (
     <div className="App">
-      <Greet 
+      {/* <Greet 
         name = 'Matt'
         massageCount = {20}
         isLoggedIn = {false}
         />
-        <Person name={personName}/>
-        <PersonList names={nameList}/>
+      <Person name={personName}/>
+      <PersonList names={nameList}/> */}
+      <Status status='error'/>
     </div>
   );
 }
